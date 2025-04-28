0a657a2b0b7a18ec4274ff57ccb7ac1bbc9d5306
kopy-kat <kopp.konrad@gmail.com>
2025-04-28 11:01:44 +0100

    feat: add get address helper

---
+++ src/get-address.ts
@@ -0,0 +1,22 @@
+import { Account, Hex } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { getChain } from "./utils/chains";
+import { getSmartAccount } from "./account";
+import { config } from "dotenv";
+
+config();
+
+export const main = async () => {
+  const owner: Account = privateKeyToAccount(
+    process.env.OWNER_PRIVATE_KEY! as Hex,
+  );
+  const targetChain = getChain("Base");
+  const targetSmartAccount = await getSmartAccount({
+    chain: targetChain,
+    owner,
+  });
+
+  console.log(`Account address: ${targetSmartAccount.account.address}`);
+};
+
+main();
